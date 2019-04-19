1375911a222ac96d77299005b097cfd8ed63b4ce
miopio <mioakasako@gmail.com>
2019-04-19 14:09:47 -0400

    case study timelines exploration

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
+//SEXUAL MISCONDUCT DOTPLOT HISTOGRAM
+
 // Set the dimensions of the canvas / graph
 var margin = {top: 10, right: 30, bottom: 30, left: 30},
-    width = 1000 - margin.left - margin.right,
-    height = 700 - margin.top - margin.bottom;
+    width = 700 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
 //parse the date
 //var parseDate = d3.timeParse("%d-%m-%Y");
@@ -31,12 +33,12 @@ var tooltip = d3.select("#chart").append("div")
     .attr("class", "tooltip")
     .style("opacity", 0);
 
-function update(){
+//function update(){
 
-var t = d3.transition()
-      .duration(1000);
+//var t = d3.transition()
+//      .duration(1000);
 
- var file = "data/sexualMisconduct_science.csv"
+var file = "data/sexualMisconduct_science.csv"
 
 // Get the data
 d3.csv(file, function(error, data) {
@@ -45,13 +47,9 @@ d3.csv(file, function(error, data) {
         d.Name = d.Name
         d.Outcome = d.Outcome
         d.Color = d.Color
-        console.log(d.Year)
-        console.log(d.Name)
-        console.log(d.Outcome);
-        console.log(d.Color);
     });
 
-console.log(data.length);
+    console.log(data.length);
     // Scale the range of the data
     //x.domain(d3.extent(data, function(d) { return d.Year; }));
     y.domain([0, data.length]);
@@ -89,6 +87,7 @@ console.log(data.length);
                   institution: p["Institution and/or Professional Society"],
                   discipline: p["Discipline or Domain"],
                   color: p.Color,
+                  link: p["Link(s)"],
                   //radius: (x(d.x1)-x(d.x0))/2
                   radius: (x(d.x1)-x(d.x0))*1.5
                 }
@@ -103,7 +102,11 @@ console.log(data.length);
         .style("fill", function(d){ return d.color; })
         //.on("mouseover", function(d, i){console.log(value[i])})
         .on("mouseover", tooltipOn)
+        //.on("click", tooltipOff)
         .on("mouseout", tooltipOff)
+        .on("click", function(d){
+          window.open(d.link)
+        })
         .transition()
           .duration(500)
           .attr("r", function(d) {
@@ -112,7 +115,7 @@ console.log(data.length);
     binContainerEnter.merge(binContainer)
         .attr("transform", d => `translate(${x(d.x0)}, ${height})`)
   });//d3.csv
-};//update
+//};//update
 
 /*function color(d){
   d3.select(this)
@@ -125,16 +128,18 @@ function tooltipOn(d) {
   let gParent = d3.select(this.parentElement)
   let translateValue = gParent.attr("transform")
 
-  let gX = translateValue.split(",")[0].split("(")[1]
+  let gX = translateValue.split(",")[0].split("(")[1] * 50
+  //let gX = translateValue.split(",")[0].split
   let gY = height + (+d3.select(this).attr("cy")- 1500)
 
   d3.select(this)
     .classed("selected", true)
-    .style("fill", "red")
+    .style("opacity", .5)
   tooltip.transition()
        .duration(200)
        .style("opacity", .9);
-  tooltip.html("<b>" + d.name + "</b>" + "</br>" + d.value + "</br>" + d.institution + "</br>" + d.discipline)
+  tooltip.html("<b>" + d.name + "</b>" + "</br>" + d.value + "</br>" + d.institution + "</br>" + d.discipline + "</br>" + "<a href= '" + d.link + "''>" + "Details" + 
+    "</a>")
   //tooltip.html("Hellohellohello")
     .style("left", gX/200 + "px")
     .style("top", gY/10 + "px")
@@ -146,6 +151,7 @@ function tooltipOff(d) {
   d3.select(this)
       .classed("selected", false)
       .style("fill", function(d){ return d.color; })
+      .style("opacity", 1)
     tooltip.transition()
          .duration(500)
          .style("opacity", 0);
@@ -160,12 +166,287 @@ svg.append("g")
   .style("stroke", "white")
   .call(d3.axisBottom(x));
 
-//draw everything
-update();
+//Legend
+    var ordinal = d3.scaleOrdinal()
+      .domain(["no action", "resigned/retired", "demoted/reprimanded", "suspended", "fired", "lawsuit settled/monetary punishment", "banned from premesis", "death"])
+      .range(["#fe0000", "#f7931e", "#f8a395", "#e41a72", "#fcd107", "#f365e7", "#a0581c", "#a90aa1", "#e6d3a5"]);
 
-//update with new data every 3sec
-/*d3.interval(function() {
-  update();
-}, 3000);*/
+    var legend = svg.append("g")
+        .attr("font-family", "futura-pt")
+        .attr("font-size", 10)
+        .attr("fill", "#fff")
+        .attr("text-anchor", "end")
+        .attr("class", ordinal)
+      .selectAll("g")
+      .enter().append("g")
+        .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+    legend.append("circle")
+        .attr("cx", 19)
+        .attr("cy", 19);
+
+    legend.append("text")
+        .attr("x", width - 24)
+        .attr("y", 9.5)
+        .attr("dy", "0.32em")
+        .text(function(d) { return d; }); 
+
+          
+
+//draw everything
+//update();
+
+
+
+//SEXUAL MISCONDUCT CASE STUDIES
+var margin1 = {top: 10, right: 30, bottom: 30, left: 30},
+    width1 = 700 - margin1.left - margin1.right,
+    height1 = 500 - margin1.top - margin1.bottom;
+
+//parse the date
+//var parseDate = d3.timeParse("%d-%m-%Y");
+var parseDate = d3.timeParse("%Y");
+
+// Set the ranges
+/*var x = d3.scaleLinear()
+    .rangeRound([0, width])
+    .domain([1980, 2018]); */
+var x1 = d3.scaleTime()
+    .rangeRound([0,width1])
+    .domain([new Date(1970, 1, 1), new Date(2018, 12, 31)])
+var y1 = d3.scaleLinear()
+    .range([height1, 0]);
+
+// Adds the svg canvas
+var svg1 = d3.select("#chart1")
+  .append("svg")
+    .attr("width", width1 + margin1.left + margin1.right)
+    .attr("height", height1 + margin1.top + margin1.bottom);
+
+// add the tooltip area to the webpage
+var tooltip1 = d3.select("#chart1").append("div")
+    .attr("class", "tooltip1")
+    .style("opacity", 0);
+
+var file1 = "data/misconduct_caseStudies.csv";
+
+var g1 = svg1.append("g")
+    .attr("transform", "translate(" + margin1.left + "," + margin1.top + ")");
+
+// Get the data
+d3.csv(file1, function(error, data) {
+    data.forEach(function(d) {
+        d.startYear = parseDate(d.Start)
+        d.endYear = parseDate(d.End)
+        d.Name = d.Name
+        d.Incident = d.Incident
+        console.log(d.startYear)
+    });
+
+console.log(data.length);
+
+    x1.domain([0, d3.max(data, function(d) { return d.End; })]);
+    //y.domain(data.map(function(d) { return d.Name; })).padding(0.1);
+
+    g1.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height1 + ")")
+        .style("stroke", "white")
+        .call(d3.axisBottom(x));
+        //.call(d3.axisBottom(x).ticks(5).tickFormat(function(d) { return parseInt(d / 1000); }).tickSizeInner([-height1]));
+
+    //g1.append("g1")
+    //    .attr("class", "y axis")
+    //    .call(d3.axisLeft(y));
+
+    g1.selectAll(".bar")
+        .data(data)
+      .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d){return (d.start)})
+        .attr("height", 100)
+        .attr("y", 100)
+        //.attr("width", function(d){ return x1(d.end - d.start)})
+        .attr("width", 2)
+        //.attr("width", function(d) { return x(d.Start); })
+        .on("mousemove", function(d){
+            tooltip1
+              .style("left", d3.event.pageX +100 + "px")
+              .style("top", d3.event.pageY - 500 + "px")
+              .style("opacity", .9)
+              .style("display", "inline-block")
+              .html((d.Name) + "<br>" + (d.Incident));
+        })
+        //.on("mouseout", function(d){ tooltip1.style("display", "none");});
+});
+
+/*svg1.append("g")
+  .attr("class", "axis axis--x")
+  .attr("transform", "translate(0," + height + ")")
+  .style("stroke", "white")
+  .call(d3.axisBottom(x)); */
+
+//TEST
+
+
+
+/*var formatDateIntoYear = d3.timeFormat("%Y");
+var formatDate = d3.timeFormat("%b %Y");
+var parseDate = d3.timeParse("%m/%d/%y");
+
+var startDate = new Date("1970-01-01"),
+    endDate = new Date("2018-12-31");
+
+var margin2 = {top:50, right:50, bottom:0, left:50},
+    width2 = 960 - margin2.left - margin2.right,
+    height2 = 500 - margin2.top - margin2.bottom;
+
+var svg2 = d3.select("#vis")
+    .append("svg")
+    .attr("width", width2 + margin.left + margin.right)
+    .attr("height", height2 + margin.top + margin.bottom);  
+
+////////// slider //////////
+
+var moving = false;
+var currentValue = 0;
+var targetValue = width2;
+
+var playButton = d3.select("#play-button");
+    
+var x = d3.scaleTime()
+    .domain([startDate, endDate])
+    .range([0, targetValue])
+    .clamp(true);
+
+var slider = svg2.append("g")
+    .attr("class", "slider")
+    .attr("transform", "translate(" + margin2.left + "," + height2/5 + ")");
+
+slider.append("line")
+    .attr("class", "track")
+    .attr("x1", x.range()[0])
+    .attr("x2", x.range()[1])
+  .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+    .attr("class", "track-inset")
+  .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+    .attr("class", "track-overlay")
+    .call(d3.drag()
+        .on("start.interrupt", function() { slider.interrupt(); })
+        .on("start drag", function() {
+          currentValue = d3.event.x;
+          update(x.invert(currentValue)); 
+        })
+    );
+
+slider.insert("g", ".track-overlay")
+    .attr("class", "ticks")
+    .attr("transform", "translate(0," + 18 + ")")
+  .selectAll("text")
+    .data(x.ticks(10))
+    .enter()
+    .append("text")
+    .attr("x", x)
+    .attr("y", 10)
+    .attr("text-anchor", "middle")
+    .text(function(d) { return formatDateIntoYear(d); });
+
+var handle = slider.insert("circle", ".track-overlay")
+    .attr("class", "handle")
+    .attr("r", 9);
+
+var label = slider.append("text")  
+    .attr("class", "label")
+    .attr("text-anchor", "middle")
+    .text(formatDate(startDate))
+    .attr("transform", "translate(0," + (-25) + ")")
+
+ 
+////////// plot //////////
+
+var dataset;
+
+var plot = svg2.append("g")
+    .attr("class", "plot")
+    .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
+
+d3.csv("data/circles.csv", prepare, function(data) {
+  dataset = data;
+  drawPlot(dataset);
+  
+  playButton
+    .on("click", function() {
+    var button = d3.select(this);
+    if (button.text() == "Pause") {
+      moving = false;
+      clearInterval(timer);
+      // timer = 0;
+      button.text("Play");
+    } else {
+      moving = true;
+      timer = setInterval(step, 100);
+      button.text("Pause");
+    }
+    console.log("Slider moving: " + moving);
+  })
+})
+
+function prepare(d) {
+  d.id = d.id;
+  d.date = parseDate(d.date);
+  return d;
+}
+  
+function step() {
+  update(x.invert(currentValue));
+  currentValue = currentValue + (targetValue/151);
+  if (currentValue > targetValue) {
+    moving = false;
+    currentValue = 0;
+    clearInterval(timer);
+    // timer = 0;
+    playButton.text("Play");
+    console.log("Slider moving: " + moving);
+  }
+}
+
+function drawPlot(data) {
+  var locations = plot.selectAll(".location")
+    .data(data);
+
+  // if filtered dataset has more circles than already existing, transition new ones in
+  locations.enter()
+    .append("circle")
+    .attr("class", "location")
+    .attr("cx", function(d) { return x(d.date); })
+    .attr("cy", height/2)
+    .style("fill", function(d) { return d3.hsl(d.date/1000000000, 0.8, 0.8)})
+    .style("stroke", function(d) { return d3.hsl(d.date/1000000000, 0.7, 0.7)})
+    .style("opacity", 0.5)
+    .attr("r", 8)
+      .transition()
+      .duration(400)
+      .attr("r", 25)
+        .transition()
+        .attr("r", 8);
+
+  // if filtered dataset has less circles than already existing, remove excess
+  locations.exit()
+    .remove();
+}
+
+function update(h) {
+  // update position and text of label according to slider scale
+  handle.attr("cx", x(h));
+  label
+    .attr("x", x(h))
+    .text(formatDate(h));
+
+  // filter data set and redraw plot
+  var newData = dataset.filter(function(d) {
+    return d.date < h;
+  })
+  drawPlot(newData);
+}*/
 
 
