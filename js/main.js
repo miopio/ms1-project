ca98799b7933e8f93619af32518d074d1ef688c3
miopio <mioakasako@gmail.com>
2019-05-20 11:05:40 -0400

    scrolly update

--- js/main.js
+++ js/main.js
@@ -9,10 +9,15 @@ var scrolly = main.select('#scrolly');
 var figure = scrolly.select('figure');
 var article = scrolly.select('article');
 var step = article.selectAll('.step');
+
+var scrolly2 = main.select('#scrolly-2');
+var figure2 = scrolly2.select('figure');
+var article2 = scrolly2.select('article');
+var step2 = article2.selectAll('.step')
 // initialize the scrollama
 var scroller = scrollama();
-var scroller1 = scrollama();
-// generic window resize listener event
+var scroller2 = scrollama();
+// generic window resize listener event : SCROLLY 1
 function handleResize() {
   // 1. update height of step elements
   var stepH = Math.floor(window.innerHeight * 0.75);
@@ -41,10 +46,37 @@ function setupStickyfill() {
     Stickyfill.add(this);
   });
 }
+
+// generic window resize listener event : SCROLLY 2
+function handleResize2() {
+  // 1. update height of step elements
+  var stepH = Math.floor(window.innerHeight * 0.75);
+  step2.style('height', stepH + 'px');
+  var figureHeight = window.innerHeight 
+  var figureMarginTop = (window.innerHeight - figureHeight) /2 
+  figure2
+    .style('height', figureHeight + 'px')
+    .style('top', figureMarginTop + 'px');
+  // 3. tell scrollama to update new element dimensions
+  scroller2.resize();
+}
+// scrollama event handlers
+function handleStepEnter2(response) {
+  console.log(response)
+  // response = { element, direction, index }
+  // add color to current step only
+  step2.classed('is-active', function (d, i) {
+    return i === response.index;
+  })
+  // update graphic based on step
+  figure2.select('p').text(response.index + 1);
+}
+
 function init() {
   setupStickyfill();
   // 1. force a resize on load to ensure proper dimensions are sent to scrollama
   handleResize();
+  handleResize2();
   // 2. setup the scroller passing options
   //    this will also initialize trigger observations
   // 3. bind scrollama event handlers (this can be chained like below)
@@ -53,15 +85,18 @@ function init() {
     offset: 0.5,
     debug: true,
   })
-
-  scroller1.setup({
-    step: '#scrolly-1 article .step',
-    offset: 0.5,
-    debug: true,
-  })
     .onStepEnter(handleStepEnter)
   // setup resize event
   window.addEventListener('resize', handleResize);
+
+  scroller2.setup({
+    step: '#scrolly-2 article .step',
+    offset: 0.5,
+    debug: true,
+  })
+    .onStepEnter(handleStepEnter2)
+  // setup resize event
+  window.addEventListener('resize', handleResize2);
 }
 // kick things off
 init();
@@ -1235,6 +1270,7 @@ var h = window.innerHeight;
                     .attr("font-family", "futura-pt")
                     .attr("font-size", "14px")
                     .style("border-color", "white")
+                    .style("margin", "5px")
                     //.attr("width",200)
                     .attr("fill", function(d){
                         if (d.Color == 0) {
@@ -1266,7 +1302,7 @@ var h = window.innerHeight;
                         return "yellow"
                       })
                       .style("stroke-width", "1px")
-                      .style("stroke-dasharray", "5, 5")
+                      .style("stroke-dasharray", "2, 2")
                       .attr("x1", function(d){return x1(d.startYear)+1})
                       .attr("y1", function(d){
                         if (d.Color == 0) {
